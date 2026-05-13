0e3a86971080626acbc585624040976e08f079f0
prakash <prakashs4u@gmail.com>
2026-05-13 22:12:25 +0530

    modules files changes

--- Modules/RG/output.tf
+++ Modules/RG/output.tf
@@ -1,6 +1,13 @@
-# output "rg_name" {
-#     value = azurerm_resource_group.rg.name
-  
-# }
+output "rg_name" {
+  value = {
+    for key, rg in azurerm_resource_group.rg :
+    key => rg.name
+  }
+}
 
-
+output "rg_location" {
+  value = {
+    for key, rg in azurerm_resource_group.rg :
+    key => rg.location
+  }
+}
